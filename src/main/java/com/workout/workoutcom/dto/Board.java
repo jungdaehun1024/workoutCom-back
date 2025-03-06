27555915cfa9b317cdeaf9528a93cf107cc8f952
jungdaehun1024 <pooh99102408@gmail.com>
2025-03-06 23:26:14 +0900

    feat: 게시글 DTO 생성

---
+++ src/main/java/com/workout/workoutcom/dto/Board.java
@@ -0,0 +1,12 @@
+package com.workout.workoutcom.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Board {
+    private String title;
+    private String content;
+    private int boardId;
+}
