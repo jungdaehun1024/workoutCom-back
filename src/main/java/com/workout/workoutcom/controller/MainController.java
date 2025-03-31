35492512ca26218a7e97441d5269a1032feba41f
jungdaehun1024 <pooh99102408@gmail.com>
2025-03-31 11:52:02 +0900

    refactor: 의존성 주입 방식 변경 +

--- src/main/java/com/workout/workoutcom/controller/MainController.java
+++ src/main/java/com/workout/workoutcom/controller/MainController.java
@@ -1,6 +1,8 @@
 package com.workout.workoutcom.controller;
 
+import com.workout.workoutcom.bean.login.RSARandBean;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 //@CrossOrigin(origins = "http://localhost:8080")  // Vue 개발 서버 포트
 @Tag(name="mainController" , description = "메인페이지에 관련한 API")
 public class MainController {
+
+    private final RSARandBean rsaRandBean;
+
+    @Autowired
+    public MainController(RSARandBean rsaRandBean) {
+        this.rsaRandBean = rsaRandBean;
+    }
 
     @GetMapping("/getPage")
     public ResponseEntity<String> getPage() {
