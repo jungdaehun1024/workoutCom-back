554c9e6babc14bba21f30064e9d6bee83f066185
jungdaehun1024 <pooh99102408@gmail.com>
2025-04-15 13:15:49 +0900

    refactor:토큰 검증 필터 수정

--- src/main/java/com/workout/workoutcom/configuration/auth/JwtAuthenticationFilter.java
+++ src/main/java/com/workout/workoutcom/configuration/auth/JwtAuthenticationFilter.java
@@ -26,20 +26,20 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws ServletException, IOException {
+        String uri = request.getRequestURI();
 
-        String token = request.getHeader("Authorization");
-        if (token != null && token.startsWith("Bearer ")) {
+        // 인증 필요 없는 URL은 필터 건너뜀
+        if (uri.startsWith("/api/auth") || uri.equals("/api/set") || uri.equals("/api/getPublicKeyModule")||uri.equals("/api/auth/loginCkeck") ) {
+            chain.doFilter(request, response);
+            return;
+        }
+        String token =JwtUtil.extractTokenFromCookie(request);
+        if (token != null && jwtUtil.validateToken(token)) {
+            String username = jwtUtil.getUsername(token);
+            UserDetails userDetails = principalDetailService.loadUserByUsername(username);
 
-            token = token.substring(7);
-            if (jwtUtil.validateToken(token)) {
-                String username = jwtUtil.getUsername(token);
-                UserDetails userDetails = principalDetailService.loadUserByUsername(username);
-
-                     UsernamePasswordAuthenticationToken authentication =
-                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-
-                SecurityContextHolder.getContext().setAuthentication(authentication);
-            }
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
         }
         chain.doFilter(request, response);
     }
