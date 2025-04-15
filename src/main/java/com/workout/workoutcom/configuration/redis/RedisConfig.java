18b35be8bd9d9cd501321b08ff775ac2f3e30666
jungdaehun1024 <pooh99102408@gmail.com>
2025-04-15 13:12:22 +0900

    feat:Redis 설정

---
+++ src/main/java/com/workout/workoutcom/configuration/redis/RedisConfig.java
@@ -0,0 +1,25 @@
+package com.workout.workoutcom.configuration.redis;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String,Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String,Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        //키는 문자열로
+        template.setKeySerializer(new StringRedisSerializer());
+
+        //값은 JSON으로 직렬화
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
+    }
+}
