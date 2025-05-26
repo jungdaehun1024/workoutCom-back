e9a91fbdb95ae8047441b6616b994e527c4f8efd
jungdaehun1024 <pooh99102408@gmail.com>
2025-05-26 11:37:33 +0900

    refactor:패스워드 변경 메소드 이동(authController , 음식조회 요청 메소드 이동(FoodSafeyController)

--- src/main/java/com/workout/workoutcom/controller/user/UserCotroller.java
+++ src/main/java/com/workout/workoutcom/controller/user/UserCotroller.java
@@ -4,7 +4,6 @@ import com.workout.workoutcom.api.foodSafety.FoodSafetyApiClient;
 import com.workout.workoutcom.api.foodSafety.dto.foodSearchJsonDto.FoodResponseDto;
 import com.workout.workoutcom.configuration.auth.PrincipalDetails;
 import com.workout.workoutcom.dto.ApiResponseDto;
-import com.workout.workoutcom.dto.user.PasswordDto;
 import com.workout.workoutcom.dto.user.UserDto;
 import com.workout.workoutcom.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,12 +16,10 @@ import org.springframework.web.bind.annotation.*;
 public class UserCotroller {
 
     private final UserService userService;
-    private final FoodSafetyApiClient foodSafetyApiClient;
 
     @Autowired
-    public UserCotroller(UserService userService, FoodSafetyApiClient foodSafetyApiClient) {
+    public UserCotroller(UserService userService) {
         this.userService = userService;
-        this.foodSafetyApiClient = foodSafetyApiClient;
     }
 
     //마이페이지 정보 조회
@@ -30,24 +27,6 @@ public class UserCotroller {
     public ResponseEntity<ApiResponseDto> getUserInfo(@AuthenticationPrincipal PrincipalDetails principalDetails) throws Exception {
         UserDto result =userService.findUserInfoByUsername(principalDetails);
         ApiResponseDto response = new ApiResponseDto(HttpStatus.OK.value(),"회원정보 조회 성공", result );
-        return ResponseEntity.ok(response);
-    }
-
-    //패스워드 변경
-    @PostMapping("/changePwd")
-    public ResponseEntity<ApiResponseDto> changePwd(@AuthenticationPrincipal PrincipalDetails principalDetails, @RequestBody PasswordDto passwordDto) throws Exception {
-        userService.changePassword(principalDetails,passwordDto);
-        ApiResponseDto response = new ApiResponseDto(HttpStatus.OK.value(), "회원 정보 변경 성공", null);
-        return ResponseEntity.ok(response);
-    }
-
-    //음식 조회 요청
-    @GetMapping("/search/foods")
-    public ResponseEntity<ApiResponseDto> searchFoodsRequest(@RequestParam("foodName") String foodName){
-        FoodResponseDto result = foodSafetyApiClient.getFoodInfo(foodName);
-
-
-        ApiResponseDto response = new ApiResponseDto(HttpStatus.OK.value(), "음식 조회 성공",result);
         return ResponseEntity.ok(response);
     }
 }
